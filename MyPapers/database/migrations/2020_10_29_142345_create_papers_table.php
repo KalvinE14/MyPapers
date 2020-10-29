567747bc909db5c4e2a8fded899087f0a228782d
jonathanchrs <61592558+jonathanchrs@users.noreply.github.com>
2020-10-29 21:34:46 +0700

    create table papers

---
+++ MyPapers/database/migrations/2020_10_29_142345_create_papers_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePapersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('papers', function (Blueprint $table) {
+            $table->id();
+            $table->text('title');
+            $table->text('type');
+            $table->longText('requirement');
+            $table->longText('description');
+            $table->text('status');
+            $table->text('file');
+            $table->text('preview');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('papers');
+    }
+}
