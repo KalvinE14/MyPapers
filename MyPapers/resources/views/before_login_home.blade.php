1242f3153c790b6aa98b7e21b37d634d92a8ccea
unknown <kalvineffendi.ke@gmail.com>
2020-12-29 00:44:37 +0700

    Update before_login_home page

--- MyPapers/resources/views/before_login_home.blade.php
+++ MyPapers/resources/views/before_login_home.blade.php
@@ -1,52 +1,18 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('template_guest.template_guest')
+@section('content')
+  <div class="container" style="margin-top: 150px; background-color: black">
+    <div class="container mt-5" style="float: left; width: 100%">
+      <h2 style="font-family: 'Poppins', sans-serif; white-space: nowrap; font-weight: 700; font-size: 48px; color: white">You write it! We make it!</h2>
+      <p style="font-family: 'Poppins', sans-serif; font-size: 22px; font-weight: 500; color: white">
+        Have a problem in writing a paper? MyPapers <br> can solve your problem. Join us to receive a <br> lot of benefits.
+      </p>
+    </div> 
+  </div>
 
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;400;500;600;700&display=swap" rel="stylesheet">
-    <title>MyPapers</title>
-  </head>
-  <body style="background-color: #293241; background-image: url(assets/bg.png); background-position: top right -200px; background-repeat: no-repeat; background-size: 60vmax; height: 100%">
-    <div class="container-md mt-3">
-        <nav class="navbar navbar-light bg-transparent">
-            <div class="container-fluid">
-              <a class="navbar-brand" href="#" style="margin-left: -20px">
-                <img src="/assets/Logo.png" alt="" class="d-inline-block align-center" style="width: 100px; height: 100px">
-                <h3 class="d-inline-block" style="margin-left: -10px; font-weight: 700; font-family: 'Poppins', sans-serif; color: white; letter-spacing: 2px">MyPapers</h3>
-              </a>
-            </div>
-          </nav>
+  <div class="container" style="margin-top: 390px;">
+    <div class="container">
+      <button type="submit" class="btn btn-danger" style="border-radius: 50px; padding: 0.5em 3em; font-family: 'Poppins', sans-serif; font-size: 18px; font-weight: 500; color: white">Register</button>
+      <a href="{{ route('login_menu') }}"><button type="button" class="btn btn-danger" style="background-color: transparent; margin-left: 24px; border: solid #d9534f; border-radius: 50px; padding: 0.5em 4em; font-family: 'Poppins', sans-serif; font-size: 18px; font-weight: 500; color: white">Login</button></a>
     </div>
-
-    <div class="container-md" style="margin-top: 150px; background-color: black">
-      <div class="container mt-5" style="float: left; width: 100%">
-        <h2 style="font-family: 'Poppins', sans-serif; white-space: nowrap; font-weight: 700; font-size: 48px; color: white">You write it! We make it!</h2>
-        <p style="font-family: 'Poppins', sans-serif; font-size: 22px; font-weight: 500; color: white">
-          Have a problem in writing a paper? MyPapers <br> can solve your problem. Join us to receive a <br> lot of benefits.
-        </p>
-      </div> 
-    </div>
-
-    <div class="container-md" style="margin-top: 390px;">
-      <div class="container">
-        <button type="button" class="btn btn-danger" style="border-radius: 50px; padding: 0.5em 3em; font-family: 'Poppins', sans-serif; font-size: 18px; font-weight: 500; color: white">Register</button>
-        <button type="button" class="btn btn-danger" style="background-color: transparent; margin-left: 24px; border: solid #d9534f; border-radius: 50px; padding: 0.5em 4em; font-family: 'Poppins', sans-serif; font-size: 18px; font-weight: 500; color: white">Login</button>
-      </div>
-    </div>
-    <!-- Optional JavaScript; choose one of the two! -->
-
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
-
-    <!-- Option 2: Separate Popper and Bootstrap JS -->
-    <!--
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>
-    -->
-  </body>
-</html>
+  </div>
+@endsection
