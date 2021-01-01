1e1dfcd82da0d838f4bd728a4a29c924c2eb3046
unknown <kalvineffendi.ke@gmail.com>
2021-01-01 20:21:28 +0700

    Fix paper history issues

--- MyPapers/resources/views/history/paper_history.blade.php
+++ MyPapers/resources/views/history/paper_history.blade.php
@@ -12,7 +12,7 @@
         <div class="container" style="margin-bottom: 50px;width: 750px; border: 5px solid white; padding: 0px 0px;">
             <div class="container" style="border-bottom: 5px solid white; width: auto;height: 50px;">
                 <div class="row" style="height: 50px;">
-                    <div class="col-lg3" style="width: auto;margin-top: auto;margin-bottom: auto; margin-left: 26px;">
+                    <div class="col-lg3" style="margin-top: auto;margin-bottom: auto; margin-left: 26px;">
                         <h4 style="color: white;">Monday, 11 January 2021</h4>
                     </div>
                 </div>
@@ -20,23 +20,23 @@
 
             <div class="container">
                 <div class="row" style="padding-top: 40px;padding-bottom: 40px; padding-left: 40px;">
-                    <div class="col-lg3" style="background-color: red; width: auto;">
+                    <div class="col-lg3" style="width: auto;">
                         <img src="{{ url('assets/previewpaper1.png') }}" alt="result paper" width="200px" height="300px">
                     </div>
 
-                    <div class="col-lg3" style="background-color: blue; width: 430px; margin-left: 10px; margin-top: auto;margin-bottom: auto;margin-right: 10px;">
-                        <div id="title" style="height: auto;margin-bottom: 10px;margin-top: 5px; padding-left: 5px;padding-right: 5px;">
+                    <div class="col-lg3" style="width: 430px; margin-left: 10px; margin-top: auto;margin-bottom: auto;margin-right: 10px;">
+                        <div id="title" style="height: auto;margin-bottom: 10px;margin-top: 5px; padding-left: 5px;padding-right: 5px;word-wrap: break-word;">
                             <h4 style="color: white;">CV FOR INTERNSHIP</h4>
                         </div>    
                    
-                        <div id="requirement" style="margin-bottom: 10px;margin-top: 5px; padding-left: 5px;padding-right: 5px;">
+                        <div id="requirement" style="margin-bottom: 10px;margin-top: 5px; padding-left: 5px;padding-right: 5px;word-wrap: break-word;">
                             <h5 style="color: white;">Requirement</h6>
                             <p style="color: white;font-weight: 100;">Simple Design</p>
                         </div>  
                         
-                        <div id="description" style="background-color: red;margin-bottom: 10px;margin-top: 5px;padding-left: 5px;padding-right: 5px;">
+                        <div id="description" style="margin-bottom: 10px;margin-top: 5px;padding-left: 5px;padding-right: 5px;word-wrap: break-word;">
                             <h5 style="color: white;">Description</h6>
-                            <p style="color: white;font-weight: 100;">Include work experience and skillaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa  aaaaaaaaaaaaaaaaa</p>
+                            <p style="color: white;font-weight: 100;">Include work experience and skills</p>
                         </div>   
 
                         <div id="type" style="margin-bottom: 10px;margin-top: 5px; padding-left: 5px;padding-right: 5px;">
@@ -44,7 +44,7 @@
                             <p style="color: white;font-weight: 100;">Curriculum Vitae</p>
                         </div>   
 
-                        <div id="type" style="height: auto;margin-bottom: 10px;margin-top: 5px; padding-left: 5px;padding-right: 5px;">
+                        <div id="type" style="margin-bottom: 10px;margin-top: 5px; padding-left: 5px;padding-right: 5px;">
                             <button type="submit" class="btn btn-danger mt-1" style="width: 120px; border-radius: 50px; font-family: 'Poppins', sans-serif; font-weight: 500; color: white;margin-bottom: 5px;">Download</button>
                         </div>   
                     </div>
