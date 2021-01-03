9af15b3876c2b5e0288d32216d88fe988281cb43
unknown <kalvineffendi.ke@gmail.com>
2021-01-03 15:39:54 +0700

    Update web.php

--- MyPapers/routes/web.php
+++ MyPapers/routes/web.php
@@ -13,49 +13,40 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('before_login_home');
-});
+Route::get('/', 'UserController@startPage')->name('start_page');
 
 Route::get('/detail', function(){
     return view('paper_detail');
 });
 
-Route::get('/update', function(){
-    return view('update_profile');
-});
-
-Route::get('/all_expert', function(){
-    return view('all_expert/all_expert');
-});
-
-Route::get('/all_paper', function(){
-    return view('all_paper/all_paper');
-});
-
-Route::get('/choose_paper', function(){
-    return view('create_paper/choose_paper');
-});
-
-Route::get('/signup', 'UserController@showSignUp');
+Route::get('/signup', 'UserController@showSignUp')->name('register_page');
 Route::post('/signup', 'UserController@doSignUp')->name('register');
-Route::get('/login_menu', 'UserController@showLoginMenu')->name('login_menu');
-Route::get('/login_user', 'UserController@showLoginUser')->name('login_user');
-Route::get('/login_expert', 'UserController@showLoginExpert')->name('login_expert');
-Route::get('/login_admin', 'UserController@showLoginAdmin')->name('login_admin');
-Route::post('/login', 'UserController@doLogin')->name('login');
+Route::get('/login/menu', 'UserController@showLoginMenu')->name('login_menu');
+Route::get('/login/user', 'UserController@showLoginUser')->name('login_user_page');
+Route::post('/login/user', 'UserController@doLoginUser')->name('login_user');
+Route::get('/all/user', 'UserController@showAllUser')->name('view_all_user');
+Route::get('/home/user', 'UserController@showHomeUser')->name('home_user');
+
+Route::get('/login/expert', 'ExpertController@showLoginExpert')->name('login_expert_page');
+Route::post('/login/expert', 'ExpertController@doLoginExpert')->name('login_expert');
+Route::get('/home/expert', 'ExpertController@showHomeExpert')->name('home_expert');
+
+Route::get('/login/admin', 'AdminController@showLoginAdmin')->name('login_admin_page');
+Route::post('/login/admin', 'AdminController@doLoginAdmin')->name('login_admin');
+Route::get('/home/admin', 'AdminController@showHomeAdmin')->name('home_admin');
 
 Route::get('/papers', 'PaperController@showAllPaperData')->name('papers');
 Route::get('/papers/{userId}', 'PaperController@showAllUserPaper')->name('papers_user');
 Route::get('/papers/pending/{userid}', 'PaperController@showPendingPapers')->name('pending_paper');
 Route::get('/papers/finished/{userid}', 'PaperController@showFinishedPapers')->name('finished_paper');
+Route::get('/paper/choose', 'PaperController@choosePaper')->name('choose_paper');
 Route::get('/paper/create_cv', 'PaperController@createCv')->name('create_paper_cv');
 Route::get('/paper/create_brochure', 'PaperController@createBrochure')->name('create_paper_brochure');
 Route::get('/paper/create_leaflet', 'PaperController@createLeaflet')->name('create_paper_leaflet');
-Route::post('/paper', 'PaperController@store');
+Route::post('/paper', 'PaperController@store')->name('create_paper');
 Route::get('/paper/paper_detail/{paper_id}/{user_id}', 'PaperController@showPaperDetail');
 Route::get('/paper/history', 'PaperController@showHistory')->name('paper_history');
 
-Route::get('/download/{preview}', 'PaperController@downloadPaper');
+Route::get('/download/{preview}', 'PaperController@downloadPaper')->name('download_paper');
 
 
